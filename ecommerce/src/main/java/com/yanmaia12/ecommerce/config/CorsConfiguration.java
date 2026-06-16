27d54acdc69c85a7cd2a004d3ead26eeaad65ce7
Yan Maia <yandantasmaia12@gmail.com>
2026-06-16 09:26:54 +0100

    feat: create CorsConfiguration

---
+++ ecommerce/src/main/java/com/yanmaia12/ecommerce/config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.yanmaia12.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("Authorization", "Content-Type", "Accept")
+                .allowCredentials(true);
+    }
+}
