0fa525334c07c1a78522bffb1da9472c94141584
Yan Maia <yandantasmaia12@gmail.com>
2026-06-17 15:52:48 +0100

    feat: create BusinessException

---
+++ ecommerce/src/main/java/com/yanmaia12/ecommerce/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.yanmaia12.ecommerce.exception;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message){
+        super(message);
+    }
+}
